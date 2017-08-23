e8bf9fc36fe44a80158f2d6cc8e7a3d47956ff03
Venkateshwar Gopisetti <venkateshwar.gopisetti@ggktech.local>
2017-08-23 13:21:43 +0530

    updated

--- titanic/nortrom.R
+++ titanic/nortrom.R
@@ -1,6 +1,22 @@
+######################################--
+#########____LIBRARIES______##########
+
 require(Amelia)
 require(ggplot2)
 require(dplyr)
+require(caret)
+require(randomForest)
+require(class)
+require(xgboost)
+require(mice)
+require(scales)
+require(ggthemes)
+
+
+#########________________###########
+
+#########____LOADING INFORMATION_____###########
+
 setwd("E:/R-Practice/titanic/")
 port <- read.csv(file = "train.csv",as.is=T,na.strings = "")
 trap <- read.csv(file = "test.csv", as.is=T,na.strings = "")
@@ -8,8 +24,16 @@ trap$Survived <- "NA"
 port$key <- "train"
 trap$key <- "test"
 comb <- rbind(port,trap)
+
+
+
+#########________________###########
+
+#########____EXTRACTING USEFUL INFORMATION____###########
+
 comb$Surname <- sapply(as.character(comb$Name), FUN=function(x) {strsplit(x, split='[,.]')[[1]][1]})
-comb$Survived <- factor(comb$Survived)
+comb$title <- sapply(as.character(comb$Name), FUN=function(x) {strsplit(x, split='[,.]')[[1]][2]})
+# comb$Survived <- as.numeric(comb$Survived)
 
 comb$SLogL <- rep(0,nrow(comb));
 # define function to compute log likelihood of a/(1-a)
@@ -21,6 +45,11 @@ logl <- function(a) {
 
 sapply(comb , function(x) sum(is.na(x))/length(x))
 missmap(comb)
+
+
+#########________________###########
+
+#########____FILLING MISSING DATA_____###########
 
 #filling in missing fare
 comb$Fare[is.na(comb$Fare)] <- mean(comb$Fare,na.rm=T)
@@ -37,7 +66,7 @@ comb$relatives <- comb$Parch+comb$SibSp
 # for()
 
 #converting Survived
-comb$Survived[comb$key=="train"] <- as.numeric(comb$Survived[comb$key=="train"])-1
+# comb$Survived[comb$key=="train"] <- as.numeric(comb$Survived[comb$key=="train"])+1
 
 #filling in loglikelihood values
 ff <- sort(unique(comb$Fare))
@@ -52,7 +81,6 @@ for (sex in c("male", "female")) {
   }
 }
 
-# 
 # ggplot(comb, aes(Sex,fill=!is.na(Cabin))) + geom_bar(position="dodge") + labs(title="Passenger Has Cabin",fill="Has Cabin")
 # 
 # ggplot(comb %>% filter(key=="train"), aes(Fare)) + 
@@ -65,7 +93,7 @@ for (sex in c("male", "female")) {
 ggplot(comb %>% filter(key=="train"), aes(x=SLogL, y=Survived)) + geom_jitter(aes(color=Survived)) +
   labs(title="SLogL vs Pclass vs TFreq")
 
- slogl1 <- comb$SLogL
+# slogl1 <- comb$SLogL
 # slogl2 <- comb$SLogL
 # comb$SLogL <- slogl2
 
@@ -73,9 +101,9 @@ ggplot(comb %>% filter(key=="train"), aes(x=SLogL, y=Survived)) + geom_jitter(ae
 ################# bias with age ########
 
 
-ggplot(comb %>% filter(key=="train"), aes(Age)) + 
-  geom_density(alpha=0.5, aes(fill=factor(Survived))) + labs(title="Survival density per Age for Pclass ") + theme(panel.grid.major = element_line(colour = "black"),panel.grid.minor = element_line(colour = "gray"))
-
+# ggplot(comb %>% filter(key=="train"), aes(Age)) + 
+#   geom_density(alpha=0.5, aes(fill=factor(Survived))) + labs(title="Survival density per Age for Pclass ") + theme(panel.grid.major = element_line(colour = "black"),panel.grid.minor = element_line(colour = "gray"))
+# 
 comb$SLogL[comb$Age<9] <- comb$SLogL[comb$Age<9]+3
 
 comb$SLogL[comb$Age>=9 & comb$Age<15] <- comb$SLogL[comb$Age>=9 & comb$Age<15]+1.2
@@ -96,44 +124,40 @@ comb$SLogL[comb$Age>=47 & comb$Age<58] <- comb$SLogL[comb$Age>=47 & comb$Age<58]
 
 comb$SLogL[comb$Age>=58 & comb$Age<67] <- comb$SLogL[comb$Age>=58 & comb$Age<67]-1.1
 
-comb$SLogL[comb$Age>=67 & comb$Age<74] <- comb$SLogL[comb$Age>=78]-1.6
+comb$SLogL[comb$Age>=67 & comb$Age<74] <- comb$SLogL[comb$Age>=67 & comb$Age<74]-1.6
 
 comb$SLogL[comb$Age>=74] <- comb$SLogL[comb$Age>=74]+0.6
 
 #################  bias with Pclass  ################# 
 
-ggplot(comb %>% filter(key=="train"), aes(Pclass)) + 
-  geom_density(alpha=0.5, aes(fill=factor(Survived))) + labs(title="Survival density per Age for Pclass ") + theme(panel.grid.major = element_line(colour = "black"),panel.grid.minor = element_line(colour = "gray"))
-
+# ggplot(comb %>% filter(key=="train"), aes(Pclass)) + 
+#   geom_density(alpha=0.5, aes(fill=factor(Survived))) + labs(title="Survival density per Age for Pclass ") + theme(panel.grid.major = element_line(colour = "black"),panel.grid.minor = element_line(colour = "gray"))
+# 
 comb$SLogL[comb$Pclass==1] <- comb$SLogL[comb$Pclass==1] + 1
 
 comb$SLogL[comb$Pclass==2] <- comb$SLogL[comb$Pclass==2] + 0.6
 
 comb$SLogL[comb$Pclass==3] <- comb$SLogL[comb$Pclass==3] -1.4
 
-
-
-
 #################  bias with Sex  #####################
 
-
-ggplot(comb %>% filter(key=="train"), aes(Sex)) + 
-  geom_bar(aes(fill=factor(Survived)),position="dodge") + labs(title="Survival density per Age for Pclass ") + theme(panel.grid.major = element_line(colour = "black"),panel.grid.minor = element_line(colour = "gray"))
-
+# ggplot(comb %>% filter(key=="train"), aes(Sex)) + 
+#   geom_bar(aes(fill=factor(Survived)),position="dodge") + labs(title="Survival density per Age for Pclass ") + theme(panel.grid.major = element_line(colour = "black"),panel.grid.minor = element_line(colour = "gray"))
+# 
 comb$SLogL[comb$Sex=="female"] <- comb$SLogL[comb$Sex=="female"] + 2.2
 
 comb$SLogL[comb$Sex=="male"] <- comb$SLogL[comb$Sex=="male"] - 3.5
 
 #################  bias with Fare ####################
 
-ggplot(comb %>% filter(key=="train"), aes(Fare)) + 
-  geom_density(alpha=0.5, aes(fill=factor(Survived))) + labs(title="Survival density per Age for Pclass ") + theme(panel.grid.major = element_line(colour = "black"),panel.grid.minor = element_line(colour = "gray"))
-# p2 <- ggplot(comb %>% filter(key=="train",Fare<50), aes(Fare)) + 
+# ggplot(comb %>% filter(key=="train"), aes(Fare)) + 
 #   geom_density(alpha=0.5, aes(fill=factor(Survived))) + labs(title="Survival density per Age for Pclass ") + theme(panel.grid.major = element_line(colour = "black"),panel.grid.minor = element_line(colour = "gray"))
-# p3 <- ggplot(comb %>% filter(key=="train",Fare>80 & Fare<120), aes(Fare)) + 
-#   geom_density(alpha=0.5, aes(fill=factor(Survived))) + labs(title="Survival density per Age for Pclass ") + theme(panel.grid.major = element_line(colour = "black"),panel.grid.minor = element_line(colour = "gray"))
-# grid.arrange(p1,p2,p3,ncol=2)
-
+# # p2 <- ggplot(comb %>% filter(key=="train",Fare<50), aes(Fare)) + 
+# #   geom_density(alpha=0.5, aes(fill=factor(Survived))) + labs(title="Survival density per Age for Pclass ") + theme(panel.grid.major = element_line(colour = "black"),panel.grid.minor = element_line(colour = "gray"))
+# # p3 <- ggplot(comb %>% filter(key=="train",Fare>80 & Fare<120), aes(Fare)) + 
+# #   geom_density(alpha=0.5, aes(fill=factor(Survived))) + labs(title="Survival density per Age for Pclass ") + theme(panel.grid.major = element_line(colour = "black"),panel.grid.minor = element_line(colour = "gray"))
+# # grid.arrange(p1,p2,p3,ncol=2)
+# 
 comb$SLogL[comb$Fare<13] <- comb$SLogL[comb$Fare<13]-2.5
 
 comb$SLogL[comb$Fare>=13 & comb$Fare<22] <- comb$SLogL[comb$Fare>=13 & comb$Fare<22]+1.2
@@ -149,35 +173,55 @@ comb$SLogL[comb$Fare>=200] <- comb$SLogL[comb$Fare>=200]+1.1
 ################## predictions  #######
 
 baratrain <- comb[comb$key=="train",c("SLogL","Survived")]
-baratrain$Survived <- as.numeric(baratrain$Survived)-1
+# baratrain$Survived <- as.numeric(baratrain$Survived)-1
 baratest <- comb[comb$key=="test",c("SLogL","Survived")]
 
 
-trControl <- trainControl(method="repeatedcv", number=7, repeats = 5); 
-fms <- formula("Survived ~ SLogL")
-model_m <- train(fms, data = comb %>% filter(key=="train"),
-                 metric="Accuracy", trControl = trControl, method = "knn"); 
-comb$Pred <- predict(model_m, comb)
-print(model_m$results)
+# trControl <- trainControl(method="repeatedcv", number=7, repeats = 5); 
+# fms <- formula("Survived ~ SLogL")
+# model_m <- train(fms, data = comb %>% filter(key=="train"),
+#                  metric="Accuracy", trControl = trControl, method = "knn"); 
+# comb$Pred <- predict(model_m, comb)
+# print(model_m$results)
 
 
 
-linear_model <- lm(Survived~SLogL,data=baratrain)
-linear_predict <- predict(linear_model,data=comb$SLogL[comb$key=="train"])
-linear_predict <- linear_predict - min(linear_predict)
-linear_predict <- linear_predict/max(linear_predict)
-thold <-0.3930461      #0.6416
-filter <- as.numeric(linear_predict>thold)
-x <- comb$Survived[comb$key=="train"]
-
-# test = comb[comb$key=="test",]
-# test_predict <- predict(linear_model, data = test)
-# thold <- 0.4040038
+# linear_model <- lm(Survived~ SLogL , data = comb %>% filter(key=="train"))
+# 
+# linear_predict <- predict(linear_model,newdata=baratrain)
+# linear_predict <- linear_predict - min(linear_predict)
+# linear_predict <- linear_predict/max(linear_predict)
+# thold <-quantile(linear_predict,0.6161616)      #0.6416
+# filter <- as.numeric(linear_predict>thold)
+# x <- comb$Survived[comb$key=="train"]
+# 
+# test_l_predict <- predict(linear_model, newdata = baratest)
 # filter <- as.numeric(test_l_predict>thold)
-# data <- data.frame(test[,1],filter)
+# data <- data.frame(trap[,1],filter)
 # colnames(data) <- c('PassengerId','Survived')
-# write.csv(data,file='l_predict3.csv',row.names=FALSE)
-
-
-
-
+# write.csv(data,file='l_predict4.csv',row.names=FALSE)
+# 
+# rf_model <- randomForest(factor(Survived) ~ SLogL, data = baratrain,ntree=200)
+# # md <- rf_model
+# pred <- predict(rf_model,newdata=baratest)
+# data <- data.frame(trap[,1],pred)
+# colnames(data) <- c('PassengerId','Survived')
+# write.csv(data,file='rf_predict4.csv',row.names=FALSE)
+# 
+# bstSparse <- xgboost(data = data.matrix(baratrain), label = baratrain$Survived, max.depth = 2, eta = 1, nthread = 2, nround = 2, objective = "binary:logistic")
+# pred<- predict(bstSparse,newdata = data.matrix(baratest))
+# pred[pred<0.5] <- 0
+# pred[pred>0.5] <- 1
+# data <- data.frame(trap[,1],pred)
+# colnames(data) <- c('PassengerId','Survived')
+# write.csv(data,file='xgb_predict1.csv',row.names=FALSE)
+# sum(x==pred)/891
+# 
+# bstSparse <- xgboost(data = data.matrix(baratrain),booster="gblinear", label = baratrain$Survived, max.depth = 2, eta = 1, nthread = 2, nround = 2, objective = "binary:logistic")
+# pred<- predict(bstSparse,newdata = data.matrix(baratest))
+# pred[pred<0.5] <- 0
+# pred[pred>0.5] <- 1
+# data <- data.frame(trap[,1],pred)
+# colnames(data) <- c('PassengerId','Survived')
+# write.csv(data,file='xgb_predict2.csv',row.names=FALSE)
+# sum(x==pred)/891
